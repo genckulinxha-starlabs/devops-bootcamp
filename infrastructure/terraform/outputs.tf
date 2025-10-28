b223377222f36d552632f4cbe2db59a04a8fce0c
unknown <osmanidriloon@gmail.com>
2025-10-28 16:53:17 +0100

    Initial commit

---
+++ infrastructure/terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "ec2_public_ip" {
+    value = aws_eip_association.eip_assoc.public_ip
+}
+
+output "ssh_connection" {
+  value = "ssh -i ~/.ssh/id_rsa ec2-user@${aws_eip.elastic_ip.public_ip}"
+}
+
+output "terraform_state_bucket" {
+  value = aws_s3_bucket.tf_state.bucket
+}
+
+output "terraform_lock_table" {
+  value = aws_dynamodb_table.tf_lock_table.name
+}
