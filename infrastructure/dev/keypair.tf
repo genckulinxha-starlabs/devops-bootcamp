8e7598869748886dd2a2ed02ba8db84a47c798b6
unknown <osmanidriloon@gmail.com>
2025-10-29 10:29:23 +0100

    Add initial GitHub Actions workflows

--- infrastructure/dev/keypair.tf
+++
@@ -1,8 +0,0 @@
-# terraform/keypair.tf
-# INFRA-2
-
-resource "aws_key_pair" "user_key" {
-  key_name   = local.key_name
-  public_key = file(var.ssh_public_key)
-}
-# The key pair resource creates an AWS key pair using the provided public key.
